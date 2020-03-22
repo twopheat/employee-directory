fabb3faf4dd9d7a9cec7fa559aedf2e47fadcf3c
twopheat <twopheat@gmail.com>
2020-03-22 13:05:56 -0700

    header size at scale

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import "../styles/Header.css";
 function Header () {
     return (
       <div className="header">
-        <h1 className="row justify-content-center"><img className="col-md-6" alt="Employee Directory" src="/empdir.png"></img></h1>
+        <h1 className="row justify-content-center"><img className="col-md-4" alt="Employee Directory" src="/empdir.png"></img></h1>
         <h5>Click on column headers to filter by heading or search for a name.</h5>
       </div>
     )
